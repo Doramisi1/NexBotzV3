db44f0568e89f7b6db77e58491b72210d3ba2487
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2023-01-13 20:34:32 +0700

    Update _autisetstatus.js

--- plugins/_autisetstatus.js
+++ plugins/_autisetstatus.js
@@ -4,7 +4,7 @@ export async function all(m) {
 	if (new Date() * 1 - setting.status > 1000) {
 		let _uptime = process.uptime() * 1000
 		let uptime = clockString(_uptime);
-		let wm = 'Panel'
+		let wm = 'Doprax'
 		let bio = `🗓️Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.self ? 'Private' : global.opts['gconly'] ? 'Hanya Grup' : 'Publik'} |  BOT MENGGUNAKAN ${wm}`
 
 		await this.setBio(bio).catch(_ => _)
