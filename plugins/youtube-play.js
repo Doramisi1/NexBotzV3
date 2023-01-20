2954bbb53bc2e7cf1222dcaa71d6a9306248657b
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2023-01-20 10:51:50 +0700

    Create youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -1,34 +1,51 @@
+import fetch from 'node-fetch'
 import fs from 'fs';
 import crypto from 'crypto'
 import { youtubeSearch } from '@bochilteam/scraper'
+let handler = async (m, { conn, groupMetadata, usedPrefix, text, args, command }) => {
+try {
+  if (!text) throw `Use example ${usedPrefix}${command} gustixa`
+  let vid = (await youtubeSearch(text)).video[0]
+  await m.reply(`*_${md} @${m.sender.split(`@`)[0]}..._*`)
+  if (!vid) throw 'Video/Audio Tidak ditemukan'
+  let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
+  const url = 'https://www.youtube.com/watch?v=' + videoId
+  let whmodsdev = `*${htki} PLAY ${htka}*
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
-let name = await conn.getName(who)
+  📌 *Title:* ${title}
+🔗 *Url:* ${url}
+📔 *Description:* ${description}
 
-   if (!text) throw `*ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ ʀᴇᴛʀɪᴇᴠᴇ ᴀᴜᴅɪᴏ ᴏʀ ᴠɪᴅᴇᴏ ғʀᴏᴍ ʏᴏᴜᴛᴜʙᴇ sᴇʀᴠᴇʀ.* 
-  
- =========================== 
- ★ ᴜsᴀɢᴇ: 
- ${usedPrefix + command} <name/url> 
-  
- ★ ᴇxᴀᴍᴩʟᴇ: 
- ${usedPrefix + command} bolenath ji` 
-   let vid = (await youtubeSearch(text)).video[0] 
-   if (!vid) throw 'ᴀᴜᴅɪᴏ ᴏʀ ᴠɪᴅᴇᴏ ɴᴏᴛ ғᴏᴜɴᴅ﹗' 
-   let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid 
-   const url = 'https://www.youtube.com/watch?v=' + videoId 
+⏲️ *Published:* ${publishedTime}
+⌚ *Duration:* ${durationH}
+👁️ *Views:* ${viewH}
+  `
+  await conn.sendButton(m.chat, whmodsdev, wm, botdate, [
+    ['🎶 Audio', `${usedPrefix}yta ${url} yes`],
+    ['🎥 Video', `${usedPrefix}ytv ${url} yes`],
+    ['🔎 Youtube Search', `${usedPrefix}yts ${text}`]
+], m, fdoc)
+} catch {
+if (!text) throw 'Input Query'
+  let vid = (await youtubeSearch(text)).video[0]
+  if (!vid) throw 'Video/Audio Tidak Ditemukan'
+  let { title, description, thumbnail, videoId, durationH, durationS, viewH, publishedTime } = vid
+  let url = 'https://www.youtube.com/watch?v=' + videoId
+  let ytLink = `https://yt-downloader.akkun3704.repl.co/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
+  let capt = `*${htki} PLAY ${htka}*
 
-    let sewa = `
- 🔖 ᴛɪᴛʟᴇ: ${title}
- 
- 📡 ᴩᴜʙʟɪsʜᴇᴅ: ${publishedTime} 
- ⌛ ᴅᴜʀᴀᴛɪᴏɴ: ${durationH} 
- 👀️ ᴠɪᴇᴡs: ${viewH} 
- 
- 📃 ᴅᴇsᴄʀɪᴩᴛɪᴏɴ: ${description} 
-`
+  📌 *Title:* ${title}
+🔗 *Url:* ${url}
+📔 *Description:* ${description}
+
+⏲️ *Published:* ${publishedTime}
+⌚ *Duration:* ${durationH}
+👁️ *Views:* ${viewH}
+  `
+  let buttons = [{ buttonText: { displayText: '🎶 Audio/Vn' }, buttonId: `${usedPrefix}yta ${url}` }, { buttonText: { displayText: '🎥 Video' }, buttonId: `${usedPrefix}ytv ${url}` }, { buttonText: { displayText: '🔎 Youtube Search' }, buttonId: `${usedPrefix}yts ${text}` }]
+  let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: capt, footer: '_Audio on progress..._', buttons }, { quoted: m })
+  // if (durationS > 4000) return conn.sendMessage(m.chat, { text: `*Download:* ${await shortUrl(ytLink)}\n\n_Duration too long..._` }, { quoted: msg })
+  conn.sendMessage(m.chat, { audio: { url: ytLink }, mimetype: 'audio/mpeg' }, { quoted: msg })
 
     let img1 = fs.readFileSync('./img/yt.png');
     let img2 = fs.readFileSync('./img/bawah.png');
@@ -45,11 +62,12 @@ let name = await conn.getName(who)
         }
     })
 }
-handler.help = ['play'].map(v => v + ' <name/url>') 
- handler.tags = ['YouTube'] 
- handler.command = /^(play)$/i 
-  
- handler.exp = 0 
- handler.limit = false 
-  
- export default handler
+handler.help = ['play', '].map(v => v + ' <pencarian>')
+handler.tags = ['YouTube']
+handler.command = /^(play)$/i
+
+handler.exp = 0
+handler.limit = false
+handler.register = true
+
+export default handler
