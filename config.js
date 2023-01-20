3863cb121fa49055065107a4e211aa0f757fbd2d
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2023-01-20 12:07:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,7 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 import moment from 'moment-timezone'
-module.exports = {
-    API_KEY_RM_BG: "6S2LsXGkd56cTLxiHdxcUnEv"
-    
+
 /*============= WAKTU =============*/
 let wibh = moment.tz('Asia/Jakarta').format('HH')
     let wibm = moment.tz('Asia/Jakarta').format('mm')
