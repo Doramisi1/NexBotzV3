b8bbcfc69515c58592f454d7bffae2032c87d385
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2023-01-21 19:37:16 +0700

    Update openai.js

--- plugins/openai.js
+++ plugins/openai.js
@@ -1,24 +1,20 @@
-const { Configuration, OpenAIApi } = require("openai");
+import { Configuration, OpenAIApi } from "openai";
 let handler = async (m, { conn, text }) => {
 if (!text) throw "[!] Masukkan teks."
-
 const configuration = new Configuration({
-  apiKey: "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"
+    apiKey: ""
 });
 const openai = new OpenAIApi(configuration);
-
-const response = await openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: text,
-  temperature: 0,
-  max_tokens: 3000,
-  top_p: 1,
-  frequency_penalty: 0.0,
-  presence_penalty: 0.0,
-  stop: ["\n"],
-});
-
-m.reply(response.data.choices[0].text)
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: text,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0
+        });
+            m.reply(response.data.choices[0].text)
     }
 handler.help = ['ai', 'openai']
 handler.tags = ['info', 'fun']
