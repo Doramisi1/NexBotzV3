5ec5d768bc523e3938913a444c8d86b390b98d3e
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2023-01-21 19:40:21 +0700

    Update openai.js

--- plugins/openai.js
+++ plugins/openai.js
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 let handler = async (m, { conn, text }) => {
 if (!text) throw "[!] Masukkan teks."
 const configuration = new Configuration({
-    apiKey: ""
+    apiKey: "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"
 });
 const openai = new OpenAIApi(configuration);
         const response = await openai.createCompletion({
